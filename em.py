6310322f3b4bda093155113b2996575b19677b3c
balescream <suryav753@gmail.com>
2019-04-23 01:32:13 +0530

    em changes

--- em.py
+++ em.py
@@ -1,8 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 
+out1=16
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(19,GPIO.OUT)
+GPIO.setup(out1,GPIO.OUT)
 switch=False
 
 try:
@@ -10,9 +11,9 @@ try:
         switch=input()
         if switch==True:
             print("off")
-            GPIO.output(19,GPIO.HIGH)
+            GPIO.output(out1,GPIO.HIGH)
         else:
             print("on")
-            GPIO.output(19,GPIO.LOW)
+            GPIO.output(out1,GPIO.LOW)
 except KeyboardInterrupt:
     GPIO.cleanup()

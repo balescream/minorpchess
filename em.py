eddf3c2ff6141a6e21e575d3bff9f010c4e3db30
balescream <suryav753@gmail.com>
2019-04-26 10:52:57 +0100

    latest code for movemotor pi

--- em.py
+++ em.py
@@ -1,13 +1,16 @@
+
 import RPi.GPIO as GPIO
 import time
 
 out1=16
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(out1,GPIO.OUT)
-def toggleMagnet(state):
-        if state:
-            print("on")
-            GPIO.output(out1,GPIO.HIGH)
-        else:
-            print("off")
-            GPIO.output(out1,GPIO.LOW)
+state=False
+while 1:
+	state=input()
+	if state:
+		print("on")
+		GPIO.output(out1,GPIO.HIGH)
+	else:
+		print("off")
+		GPIO.output(out1,GPIO.LOW)
